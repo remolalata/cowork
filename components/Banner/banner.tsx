2fa25ea435ba9896c2a29f3a1ca4279a4d80531a
remolalata <lalata.remo@gmail.com>
2024-04-26 11:25:15 +0800

    banner component

--- components/Banner/banner.tsx
+++ components/Banner/banner.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 import Button from "../UI/Button/button";
+import { BANNER_CTA, BANNER_HERO_IMAGE_ALT, BANNER_HERO_SRC, STANDFIRST_LABEL } from "@/constants/siteLabelsConstants";
 
 const Banner: React.FC = () => {
     return (
@@ -9,16 +10,16 @@ const Banner: React.FC = () => {
                 <h1 className="font-neue-regrade font-bold text-5xl lg:text-8xl leading-tight lg:leading-tight">
                     Elevate Your Workspace with <span className="relative z-20 inline-block before:content-[''] before:absolute before:-inset-x-2.5 before:-inset-y-2.5 before:bg-hero-title-pattern before:bg-no-repeat before:bg-center before:bg-contain before:z-10"><span className="relative z-20">Cowork</span></span>
                 </h1>
-                <p className="mt-6 lg:mt-8 leading-6">Welcome to Cowork – where innovation meets collaboration in the heart  of productivity! Unleash 🚀 your potential in our thoughtfully designed coworking spaces, tailored to inspire creativity and foster connections.</p>
+                <p className="mt-6 lg:mt-8 leading-6">{STANDFIRST_LABEL}</p>
                 <div className="w-fit mx-auto lg:ml-0 mt-10 lg:mt-12">
-                    <Button>Claim Your Spot</Button>
+                    <Button>{BANNER_CTA}</Button>
                 </div>
             </div>
             <div className="w-full">
                 <div className="relative w-full lg:w-[640px] h-[320px] lg:h-[640px] rounded-3xl lg:rounded-[48px] overflow-hidden">
                     <Image
-                        src={"/images/hero.jpg"}
-                        alt="Two people having a meeting in a modern office lounge with stylish seating and vibrant decor."
+                        src={BANNER_HERO_SRC}
+                        alt={BANNER_HERO_IMAGE_ALT}
                         fill
                         priority
                         className="object-cover"
---
+++ constants/siteLabelsConstants.ts
@@ -0,0 +1,4 @@
+export const STANDFIRST_LABEL: string = "Welcome to Cowork – where innovation meets collaboration in the heart  of productivity! Unleash 🚀 your potential in our thoughtfully designed coworking spaces, tailored to inspire creativity and foster connections.";
+export const BANNER_HERO_SRC: string = "/images/hero.jpg";
+export const BANNER_HERO_IMAGE_ALT = "Two people having a meeting in a modern office lounge with stylish seating and vibrant decor.";
+export const BANNER_CTA: string = "Claim Your Spot";
